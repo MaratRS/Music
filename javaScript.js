00416528d420d960c51b1dc97493f175d7d6fedd
MaratRS <60040183+MaratRS@users.noreply.github.com>
2022-10-07 02:52:28 +0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+const slides = document.querySelectorAll('.slide')
+slides.forEach((elem) => {
+    elem.onclick = () => {
+        document.querySelector('.slide.active')?.classList.remove('active')
+    elem.classList.add('active')
+    }
+})
+
+ 
